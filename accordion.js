98f435f6c55eec954e1eb901446ea8425790450e
lenemter <lenemter@gmail.com>
2022-07-15 23:51:43 +0900

    Accordion

--- accordion.js
+++ accordion.js
@@ -1,14 +1,24 @@
-const accordion_items = document.querySelectorAll(".accordion > .item");
+function close_all(exception) {
+    const active_accordion_items = document.querySelectorAll(".accordion > .item.active");
+    for (const item of active_accordion_items) {
+        if (item != exception) {
+            item.classList.remove("active");
+            const content = item.querySelector(".content");
+            content.style.maxHeight = null;
+        }
+    }
+}
 
+const accordion_items = document.querySelectorAll(".accordion > .item");
 for (const item of accordion_items) {
     item.onclick = () => {
-        const label = item.querySelector(".label");
         const content = item.querySelector(".content");
 
-        item.classList.toggle("active");
+        close_all(exception = item);
+        item.classList.toggle("active")
 
         if (content.style.maxHeight) {
-            content.style.maxHeight = 0;
+            content.style.maxHeight = null;
         }
         else {
             content.style.maxHeight = content.scrollHeight + "px";
